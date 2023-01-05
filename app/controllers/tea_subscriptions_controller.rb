26d53d215e30cc216606b89b3063f6b50d64e70f
Gavin Carew <gjcarew@gmail.com>
2023-01-04 21:29:14 -0800

    feat: model setup

---
+++ app/controllers/tea_subscriptions_controller.rb
@@ -0,0 +1,2 @@
+class TeaSubscriptionsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :tea_subscriptions
+  resources :teas
+  resources :customer_subscriptions
+  resources :subscriptions
+  resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
