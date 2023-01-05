14ea1564fcbc75dcab6a6e5ae292804f3e18b87c
Gavin Carew <gjcarew@gmail.com>
2023-01-05 09:42:18 -0800

    Error handling and setting up API docs

--- spec/requests/api/v1/customer_subscriptions_spec.rb
+++ spec/requests/api/v1/customer_subscriptions_spec.rb
@@ -14,8 +14,15 @@ RSpec.describe 'Customer subscription API', type: :request do
         let (:customer) { create(:customer) }
         let (:customer_id) { customer.id }
         before { create_list(:customer_subscription, 3, customer: customer)}
-
         include_context 'with integration test'
+      end
+
+      response '404', 'Customer not found' do 
+        let (:customer_id) { 4 }
+        run_test! do |response|
+          not_found = JSON.parse(response.body, symbolize_names: true)
+          expect(not_found[:error]).to eq("Couldn't find Customer with 'id'=4")
+        end
       end
     end
   end
